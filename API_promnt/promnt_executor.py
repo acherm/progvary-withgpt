cf6a25e1c1332afade9b820bbc4fbea5a7ca980c
José A. Galindo <jagalindo@us.es>
2023-04-15 00:23:54 +0200

    printing results

--- API_promnt/promnt_executor.py
+++ API_promnt/promnt_executor.py
@@ -5,7 +5,7 @@ import time
 import random
 import os
 import openai
-
+import json
 parser = argparse.ArgumentParser()
 parser.add_argument("numberofpromnts")
 parser.add_argument("path")
@@ -41,6 +41,16 @@ def generate_promnt(input_file,output_dir,i):
     destination_file.close()
     time.sleep(random.randint(20,40))
 
+def print_json(entry, outputdir, i):
+    json_file_path=os.path.join(outputdir,os.path.basename(entry)+'_r_'+str(i))
+    with open(json_file_path) as json_file:
+        json_data = json.load(json_file)
+        data_to_insert=json_data['choices'][0]['text']
+        #destinationFile = open(os.path.join(DESTINATION_DIR,str(x)+'_'+filename),'w')
+        #print(''.join(result), file = destinationFile)
+        #destinationFile.close()
+        print(data_to_insert)
+
 if not os.path.exists(outputdir):
    # Create a new directory because it does not exist
    os.makedirs(outputdir)
@@ -52,5 +62,5 @@ if not target_dir.exists():
 for entry in target_dir.iterdir():
     if entry.is_file():
         for i in range(numberofpromnts):
-            generate_promnt(entry, outputdir, i)
-
+            #generate_promnt(entry, outputdir, i)
+            print_json(entry,outputdir,i)
